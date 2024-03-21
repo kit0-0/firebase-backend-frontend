227b62d5f58c4e0a27266a93ad44dcb8c8fd5042
kirti arora <111427307+kit0-0@users.noreply.github.com>
2024-03-22 05:12:12 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,7 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAEeJ061SU-S0moQfbCDhmDaj6c9iw0W74",
-  authDomain: "tutorial-8b205.firebaseapp.com",
-  databaseURL: "https://tutorial-8b205-default-rtdb.firebaseio.com",
-  projectId: "tutorial-8b205",
-  storageBucket: "tutorial-8b205.appspot.com",
-  messagingSenderId: "706605247577",
-  appId: "1:706605247577:web:3213b9ebb248d6d094d548"
+  //
 };
 
 
